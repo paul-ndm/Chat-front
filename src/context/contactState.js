f7c7a7f2c92a232f4eb79ec02803c199bd2a06f5
Paul <n.d.mont.p@gmail.com>
2021-05-03 16:45:53 +0200

    private message2

--- src/context/contactState.js
+++ src/context/contactState.js
@@ -10,55 +10,46 @@ export const ContactState = ({children}) => {
 
     const [contacts, setContacts] = useState([])
     const [selectedContactIndex, setSelectedContactIndex] = useState(0)
-    const [selectedContact, setSelectedContact ] = useState()
+    //const [selectedContact, setSelectedContact ] = useState()
 
        // switch selected-property
    useEffect(()=> {
-
-    console.log('hi')
-    
-        const newContacts = contacts.map((contact, index) => {
+    const newContacts = contacts.map((contact, index) => {
       const { id, name, messages } = contact
       const selected = index === selectedContactIndex
       const newContact = { id, name, messages, selected}
-      if(newContact.selected) {
-        setSelectedContact(newContact)
-      }
+    //   if(newContact.selected) {
+    //     setSelectedContact(newContact)
+    //   }
       return newContact
     })
     setContacts(newContacts)
 
    }, [selectedContactIndex])
 
+   // add new Contact
     const createContact = (id, name) => {
 
         const newContact = { id, name, messages: [], selected: true}
-        setSelectedContact(newContact)
-
-        if(contacts !== []) {
-            
-            setContacts(prev => {
-                const newContact = { id, name, messages: [], selected: true}
-                const oldContacts = prev.map( event => 
-                    {
-                    const { id, name, messages } = event
-                    return { id, name, messages, selected: false}
-                  })
-                  setSelectedContactIndex(oldContacts.lenght)
-
-                return [...oldContacts, newContact]
-            })
-        } else {
-            setContacts(newContact)
-        }
-
+        //setSelectedContact(newContact)
+        setContacts(prev => {
+            const oldContacts = prev.map( event => 
+                {
+                const { id, name, messages } = event
+                return { id, name, messages, selected: false}
+              })
+            return [...oldContacts, newContact]
+        })
+        setSelectedContactIndex(contacts.length)
 
     } 
 
 
 
     return (
-        <ContactContext.Provider value={{contacts, createContact, selectedContact,setSelectedContact, setSelectedContactIndex}}>
+        <ContactContext.Provider value={{contacts, createContact, 
+            // selectedContact,setSelectedContact, 
+            selectedContactIndex, setSelectedContactIndex}}>
             {children}
         </ContactContext.Provider>
     );
